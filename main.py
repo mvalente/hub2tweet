2a352e78e6456ab1b6b11ff784c38c7fc5323d39
naze.sawyer <naze.sawyer@ca504464-d649-11de-89a3-7d4a0ca7bf7e>
2009-11-21 18:36:30 +0000

    Added a template for Hello World!

--- main.py
+++ main.py
@@ -2,14 +2,15 @@
 
 import wsgiref.handlers
 
-
+from google.appengine.ext.webapp import template
 from google.appengine.ext import webapp
 
 
 class MainHandler(webapp.RequestHandler):
 
   def get(self):
-    self.response.out.write('Hello world!')
+    text = template.render('templates/index.tpl', {})
+    self.response.out.write(text)
 
 
 def main():
