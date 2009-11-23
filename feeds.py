29ccfacb8206ac65b357786bf356ae5ed64300aa
naze.sawyer <naze.sawyer@ca504464-d649-11de-89a3-7d4a0ca7bf7e>
2009-11-23 21:56:02 +0000

    modified get_hub_from_feed function to make unit test pass...coding used document and element objects to determine if a feed has a <link rel='hub'> tag.  If True, returns href.

--- feeds.py
+++ feeds.py
@@ -4,11 +4,16 @@ import xml.dom.minidom
 
 def get_hub_from_feed(xml_doc):
   """If a feed has a <link rel='hub'> tag, return the href."""
-
+  
   # If the parameter is a string, convert to a Document.
   if type(xml_doc) is str:
     xml_doc = xml.dom.minidom.parseString(xml_doc)
-
+  elements = xml_doc.getElementsByTagName('link')
+  for i in elements:
+    if i.hasAttribute('rel')==True:
+      if i.getAttribute('rel') == 'hub':
+        href = i.getAttribute('href')
+  return href
   # TODO(sawyer): Implement.
   # At this point, xml_doc is a Document.
   # See http://docs.python.org/library/xml.dom.html#document-objects
