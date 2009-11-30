51125a66861c31b35952e4a37c6167f50f15dc35
naze.sawyer <naze.sawyer@ca504464-d649-11de-89a3-7d4a0ca7bf7e>
2009-11-30 20:44:15 +0000

    fixed comments from nathan

--- branches/sawyer-review/feeds.py
+++ branches/sawyer-review/feeds.py
@@ -2,7 +2,15 @@
 
 import xml.dom.minidom
 
-def get_href_from_feed(xml_doc, rel_type):
+def get_hub(xml_doc):
+  """If a feed has a <link rel='hub'> tag, returns the href."""
+  return _get_href_from_feed(xml_doc, 'hub')
+
+def get_self(xml_doc):
+  """If a feed has a <link rel='self'> tag, return the href."""
+  return _get_href_from_feed(xml_doc, 'self')
+  
+def _get_href_from_feed(xml_doc, rel_type):
   """If a feed has a <link rel='hub'> or <link rel='self'> tag, returns the href"""
     if type(xml_doc) is str:
         xml_doc = xml.dom.minidom.parseString(xml_doc)
@@ -13,12 +21,3 @@ def get_href_from_feed(xml_doc, rel_type):
             if i.getAttribute('rel') == rel_type:
                 href = i.getAttribute('href')
     return href
-                
-def get_hub(xml_doc):
-  """If a feed has a <link rel='hub'> tag, returns the href."""
-  return get_href_from_feed(xml_doc, 'hub')
-
-def get_self(xml_doc):
-  """If a feed has a <link rel='self'> tag, return the href."""
-  return get_href_from_feed(xml_doc, 'self')
-  
