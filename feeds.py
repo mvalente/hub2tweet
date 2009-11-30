23562508f6099f6673c81524a2bd2f50c023c8c7
naze.sawyer <naze.sawyer@ca504464-d649-11de-89a3-7d4a0ca7bf7e>
2009-11-30 20:54:37 +0000

    Implemented get_self and _get_href_from_feed functions

--- feeds.py
+++ feeds.py
@@ -2,21 +2,22 @@
 
 import xml.dom.minidom
 
-def get_hub_from_feed(xml_doc):
-  """If a feed has a <link rel='hub'> tag, return the href."""
-  if type(xml_doc) is str:
-    xml_doc = xml.dom.minidom.parseString(xml_doc)
-  # At this point, xml_doc is a Document.
-  elements = xml_doc.getElementsByTagName('link')
-  for i in elements:
-    if i.hasAttribute('rel'):
-      if i.getAttribute('rel') == 'hub':
-        href = i.getAttribute('href')
-  return href
+def get_hub(xml_doc):
+  """If a feed has a <link rel='hub'> tag, returns the href."""
+  return _get_href_from_feed(xml_doc, 'hub')
 
-def get_self_from_feed(xml_doc):
+def get_self(xml_doc):
   """If a feed has a <link rel='self'> tag, return the href."""
-  # TODO(sawyer): Implement.  Note, you could make a third helper function that
-  # takes xml_doc and a rel type and then get_hub and get_self become tiny
-  # wrappers.
+  return _get_href_from_feed(xml_doc, 'self')
   
+def _get_href_from_feed(xml_doc, rel_type):
+  """If a feed has a <link rel='hub'> or <link rel='self'> tag, returns the href"""
+    if type(xml_doc) is str:
+        xml_doc = xml.dom.minidom.parseString(xml_doc)
+    # At this point, xml_doc is a Document.
+    elements = xml_doc.getElementsByTagName('link')
+    for i in elements:
+        if i.hasAttribute('rel'):
+            if i.getAttribute('rel') == rel_type:
+                href = i.getAttribute('href')
+    return href
